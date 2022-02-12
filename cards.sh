637c2b8f7c6aa996e499eac63afd9d8d907253a8
noname774300 <noname774300@gmail.com>
2022-02-13 08:51:24 +0900

    delete translating...

--- cards.sh
+++ cards.sh
@@ -9,7 +9,6 @@ sleep 3
 filename="$(echo ${word} | awk '{ gsub(/[^A-Za-z0-9]/, "_"); print }').txt"
 filepath="${dirpath}/${filename}"
 if [ ! -e ${filepath} ]; then
-echo "translating..."
 trans -d -j en:ja ${word} > ${filepath} 2>&1
 fi
 less -f -R ${filepath}
--- trans.sh
+++ trans.sh
@@ -6,7 +6,6 @@ mkdir -p ${dirpath}
 filename="$(echo ${@} | awk '{ gsub(/[^A-Za-z0-9]/, "_"); print }').txt"
 filepath="${dirpath}/${filename}"
 if [ ! -e ${filepath} ]; then
-echo "translating..."
 trans ${@} > ${filepath} 2>&1
 fi
 less -R ${filepath}
