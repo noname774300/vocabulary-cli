58732e90453aae35bbcbaa6bea5b26faa90d1c0a
noname774300 <noname774300@gmail.com>
2022-02-14 20:37:31 +0900

    overwrite files if multibyte characters are passed

--- cards.sh
+++ cards.sh
@@ -8,7 +8,7 @@ echo $word
 sleep 3
 filename="$(echo ${word} | awk '{ gsub(/[^A-Za-z0-9]/, "_"); print }').txt"
 filepath="${dirpath}/${filename}"
-if [ ! -e ${filepath} ]; then
+if [ ! -e ${filepath} ] || [[ ${filepath} =~ _{3,} ]]; then
 trans -d -j en:ja ${word} > ${filepath} 2>&1
 fi
 less -f -R ${filepath}
--- trans.sh
+++ trans.sh
@@ -5,7 +5,7 @@ mkdir -p ${dirpath}
 
 filename="$(echo ${@} | awk '{ gsub(/[^A-Za-z0-9]/, "_"); print }').txt"
 filepath="${dirpath}/${filename}"
-if [ ! -e ${filepath} ]; then
+if [ ! -e ${filepath} ] || [[ ${filepath} =~ _{3,} ]]; then
 trans ${@} > ${filepath} 2>&1
 fi
 less -R ${filepath}
